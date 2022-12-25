602b7cd45e8bfcef5e46e29aebbd2bcfa181a373
Sebastian Perez <spezuluaga@gmail.com>
2022-12-25 16:57:12 -0500

    code - express using json middleware

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,8 @@ import express from 'express';
 
 const app = express();
 
+app.use(express.json());
+
 const PORT = 3000;
 app.listen(PORT, () => {
   console.log(`Server listening on port: ${PORT}`);
