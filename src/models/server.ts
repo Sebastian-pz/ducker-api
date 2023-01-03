a5fa5dbac9d511be6654ec0c3b32eb6949767cb8
Matías Straface <matiasgstraface@gmail.com>
2023-01-03 16:43:15 -0300

    restructure-express

---
+++ src/models/server.ts
@@ -0,0 +1,50 @@
+import express from 'express';
+import dbConnection from '../database/config';
+
+class Server {
+	port: string | undefined;
+	app: any;
+	paths: { users: string; auth: string; cuack: string };
+
+	constructor() {
+		this.app = express();
+		this.port = process.env.PORT;
+
+		this.paths = {
+			users: '/users',
+			auth: '/auth',
+			cuack: '/cuack',
+		};
+
+		//conexion a base de datos
+		this.conectarDB();
+
+		//middlewares
+		this.middlewares();
+
+		//rutas
+		//this.routes();
+	}
+
+	conectarDB = async () => {
+		await dbConnection();
+	};
+
+	middlewares() {
+		this.app.use(express.json());
+	}
+
+	// routes() {
+	// 	this.app.use(this.paths.auth, require('../routes/auth'));
+	// 	this.app.use(this.paths.users, require('../routes/user'));
+	// 	this.app.use(this.paths.cuack, require('../routes/cuack'));
+	// }
+
+	listen() {
+		this.app.listen(this.port, () => {
+			console.log(` 🦆 Server listening on port: ${this.port} 🦆 `);
+		});
+	}
+}
+
+export default Server;
