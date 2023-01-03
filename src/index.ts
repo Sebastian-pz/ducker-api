2279615f33dd754de883c688ab762f6797aaa2c9
Matías Straface <matiasgstraface@gmail.com>
2023-01-03 12:23:36 -0300

    code/base de datos conectada

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,23 @@
 import express from 'express';
+import dbConnection from '../src/database/config';
+import * as dotenv from 'dotenv';
+dotenv.config();
 
 import exampleRoutes from './routes/example';
 
 const app = express();
 app.use(express.json());
 
+const conectarDB = async () => {
+	await dbConnection();
+};
+conectarDB();
+
 // Routes section
 app.use('/api/example', exampleRoutes);
 
 //Start server
-const PORT = 3001;
+let PORT = process.env.PORT ? process.env.PORT : 5000;
 app.listen(PORT, () => {
-  console.log(
-    `🎄🎄🎄🎄🎄🎄🎄🎄🎄🎄🎄🎄🎄🎄🎄🎄🎄🎄🎄🎄\nServer listening on port: ${PORT}\n🎄🎄🎄🎄🎄🎄🎄🎄🎄🎄🎄🎄🎄🎄🎄🎄🎄🎄🎄🎄`
-  );
+	console.log(` 🦆 Server listening on port: ${PORT} 🦆 `);
 });
