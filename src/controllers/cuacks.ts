73068795eec6570b1aa437c7be8d2de05ca7d9e8
Matias Straface <93721908+mgstraface@users.noreply.github.com>
2023-01-09 12:41:21 -0300

    Code/cuacks controllers, routes

--- src/controllers/cuacks.ts
+++ src/controllers/cuacks.ts
@@ -3,90 +3,122 @@ import User from '../models/user';
 import addComment from './utils/addComment';
 import addLike from './utils/addLike';
 import addRecuacks from './utils/addRecuacks';
+import { noSpecialCharactersContent } from '../middlewares/utils/fields';
 
 export const cuackPost = async (req: Request, res: Response) => {
-  const { cuack } = req.body;
+	const { cuack } = req.body;
 
-  if (!cuack)
-    return res.status(400).send({ response: false, msg: 'Missing info' });
+	if (!cuack) return res.status(400).send({ response: false, msg: 'Missing info' });
 
-  try {
-    User.findOneAndUpdate(
-      { _id: cuack.author },
-      { $push: { cuacks: cuack } },
-      function (error: any, _success: any) {
-        if (error) {
-          return res
-            .status(400)
-            .send({ response: false, msg: 'Post was not published' });
-        }
-        return res.status(202).send({ response: true, msg: 'Post published' });
-      }
-    );
-  } catch (error) {
-    console.log(`Error posting a cuack ${error}`);
-    return res
-      .status(500)
-      .send({ response: false, msg: 'Internal server error' });
-  }
+	if (!noSpecialCharactersContent(cuack.content)) {
+		return res.status(400).send({ response: false, msg: 'Special characters is not allowed.' });
+	}
 
-  return;
+	try {
+		User.findOneAndUpdate(
+			{ _id: cuack.author },
+			{ $push: { cuacks: cuack } },
+			function (error: any, _success: any) {
+				if (error) {
+					return res.status(400).send({ response: false, msg: 'Post was not published' });
+				}
+				return res.status(202).send({ response: true, msg: 'Post published' });
+			}
+		);
+	} catch (error) {
+		console.log(`Error posting a cuack ${error}`);
+		return res.status(500).send({ response: false, msg: 'Internal server error' });
+	}
+
+	return;
 };
 
 //likes, comments & recuacks
 export const cuackPut = async (req: Request, res: Response) => {
-  const id = req.params.id;
-  const { likeID, comment, cuackID, recuackID } = req.body;
+	const id = req.params.id;
+	const { likeID, comment, cuackID, recuackID } = req.body;
 
-  if (id && likeID && cuackID) {
-    const response = await addLike(id, likeID, cuackID);
-    if (response) {
-      return res
-        .status(202)
-        .send({ response: true, msg: 'Likes updated successfully' });
-    } else {
-      return res
-        .status(500)
-        .send({ response: false, msg: 'Error in like post' });
-    }
-  }
+	if (id && likeID && cuackID) {
+		const response = await addLike(id, likeID, cuackID);
+		if (response) {
+			return res.status(202).send({ response: true, msg: 'Likes updated successfully' });
+		} else {
+			return res.status(500).send({ response: false, msg: 'Error in like post' });
+		}
+	}
 
-  if (id && comment && cuackID) {
-    const response = await addComment(id, cuackID, comment);
-    if (response) {
-      return res
-        .status(202)
-        .send({ response: true, msg: 'Comment updated successfully' });
-    } else {
-      return res
-        .status(500)
-        .send({ response: false, msg: 'Error, you could comment' });
-    }
-  }
+	if (id && comment && cuackID) {
+		const response = await addComment(id, cuackID, comment);
+		if (response) {
+			return res.status(202).send({ response: true, msg: 'Comment updated successfully' });
+		} else {
+			return res.status(500).send({ response: false, msg: 'Error, you could comment' });
+		}
+	}
 
-  if (id && recuackID && cuackID) {
-    const response = await addRecuacks(id, recuackID, cuackID);
-    if (response) {
-      return res
-        .status(202)
-        .send({ response: true, msg: 'Recuack updated successfully' });
-    } else {
-      return res
-        .status(500)
-        .send({ response: false, msg: 'Error in recuack post' });
-    }
-  }
-  return res.status(400).send({ response: false, msg: 'Missing information' });
+	if (id && recuackID && cuackID) {
+		const response = await addRecuacks(id, recuackID, cuackID);
+		if (response) {
+			return res.status(202).send({ response: true, msg: 'Recuack updated successfully' });
+		} else {
+			return res.status(500).send({ response: false, msg: 'Error in recuack post' });
+		}
+	}
+	return res.status(400).send({ response: false, msg: 'Missing information' });
 };
 
-// export const cuackDelete = async (req: Request, res: Response) => {
-// 	const id = req.user;
-// 	const { cuackID } = req.body;
+export const cuacksByUser = async (req: Request, res: Response) => {
+	const { since = '0', limit = '10' } = req.query;
+	const { id } = req.params;
+	let s: number = 0;
+	let l: number = 10;
 
-// 	try {
-// 		const user = await User.findOne({ _id: id });
-// 	} catch (error) {
-// 		console.log(`cuackDelete internal error: ${error}`);
-// 		return res.status(500).send({ msg: 'Internal server error' });
-// 	}
-// };
+	if (since && typeof since === 'string') s = parseInt(since);
+	if (limit && typeof limit === 'string') l = parseInt(limit);
+
+	try {
+		const user = await User.findOne({ id });
+
+		if (!user) return res.status(400).send({ response: false, msg: 'Invalid ID or missing info' });
+
+		const total = user.cuacks.length;
+		const cuacks = user.cuacks.slice(s, l);
+		return res.status(200).send({ total, cuacks });
+	} catch (error) {
+		console.log(`Error getting cuacks, ${error}`);
+		return res.status(500).send({ response: false, msg: 'Internal server error' });
+	}
+};
+
+export const cuacksByID = async (req: Request, res: Response) => {
+	try {
+		const id = req.params.id;
+		const { cuackID } = req.body;
+
+		const user = await User.findById(id);
+		// @ts-ignore
+		const userCuack = user && user.cuacks.find((cuack) => cuack.id === cuackID);
+		return res.status(201).send({ response: true, userCuack });
+	} catch (error) {
+		console.log(`Error searching a cuack: ${error}`);
+		return res.status(500).send({ response: false, msg: 'Internal server error' });
+	}
+};
+
+export const cuackDelete = async (req: Request, res: Response) => {
+	const id = req.params.id;
+	const { cuackID } = req.body;
+
+	try {
+		await User.findOneAndUpdate(
+			{ id, 'cuacks._id': cuackID },
+			{ 'cuacks.$.isPublic': false },
+			{ new: true }
+		);
+
+		return res.status(202).send({ response: true, msg: 'Cuack deleted successfully' });
+	} catch (error) {
+		console.log(`cuackDelete internal server error: ${error}`);
+		return res.status(500).send({ msg: 'Internal server error' });
+	}
+};
--- src/middlewares/utils/fields.ts
+++ src/middlewares/utils/fields.ts
@@ -1,19 +1,25 @@
 export function noBanWords(nickname: string): boolean {
-  const banWords: string[] = [' ', 'admin', 'ducker', 'staff', 'moderator'];
-  let isValid: boolean = true;
+	const banWords: string[] = [' ', 'admin', 'ducker', 'staff', 'moderator'];
+	let isValid: boolean = true;
 
-  banWords.forEach((word: string) => {
-    if (nickname.toLowerCase().includes(word)) {
-      isValid = false;
-      return;
-    }
-  });
+	banWords.forEach((word: string) => {
+		if (nickname.toLowerCase().includes(word)) {
+			isValid = false;
+			return;
+		}
+	});
 
-  return isValid;
+	return isValid;
 }
 
 export function noSpecialCharacters(input: string): boolean {
-  const validation = [...input.matchAll(/[A-Za-z0-9]/g)];
-  if (validation.length < input.length) return false;
-  return true;
+	const validation = [...input.matchAll(/[A-Za-z0-9]/g)];
+	if (validation.length < input.length) return false;
+	return true;
+}
+// ü ï ää ë üëö Ä Ë Ï Ö Ü
+export function noSpecialCharactersContent(input: string): boolean {
+	const validation = [...input.matchAll(/[{}]/g)];
+	if (validation.length) return false;
+	return true;
 }
