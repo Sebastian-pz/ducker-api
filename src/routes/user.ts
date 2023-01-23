74774710393688ac7a814740d1bc76c32bad499b
Sebastian Perez <spezuluaga@gmail.com>
2023-01-23 11:12:59 -0500

    feature query users, autocomplete component

--- src/routes/user.ts
+++ src/routes/user.ts
@@ -2,29 +2,30 @@ import express from 'express';
 import { check } from 'express-validator';
 import { usersMailsAndNicknames } from '../controllers/user';
 import {
-	validateFields,
-	validateNickname,
-	validatePassword,
-	validateJWT,
-	existEmail,
-	existNickname,
-	existUser,
-	compareJwtInfoAndParamID,
+  validateFields,
+  validateNickname,
+  validatePassword,
+  validateJWT,
+  existEmail,
+  existNickname,
+  existUser,
+  compareJwtInfoAndParamID,
 } from '../middlewares';
 
 import {
-	usersGet,
-	userPost,
-	userID,
-	usersPut,
-	userDelete,
-	userFollowing,
-	userFollowers,
-	userSilenced,
-	userBlocked,
-	userUnfollowing,
-	userRemoveSilenced,
-	userRemoveBlocked,
+  usersGet,
+  userPost,
+  userID,
+  usersPut,
+  userDelete,
+  userFollowing,
+  userFollowers,
+  userSilenced,
+  userBlocked,
+  userUnfollowing,
+  userRemoveSilenced,
+  userRemoveBlocked,
+  queryUsers,
 } from '../controllers';
 
 const router = express.Router();
@@ -32,96 +33,133 @@ const router = express.Router();
 router.get('/', usersGet);
 
 router.get(
-	'/:id',
-	[check('id', 'Invalid ID').isMongoId(), check('id').custom(existUser), validateFields],
-	userID
+  '/:id',
+  [
+    check('id', 'Invalid ID').isMongoId(),
+    check('id').custom(existUser),
+    validateFields,
+  ],
+  userID
 );
 
 router.post(
-	'/',
-	[
-		check('email', 'Invalid email :D').isEmail(),
-		check('email').custom(existEmail),
-		check('fullname', 'Full name is required').not().isEmpty(),
-		check('nickname', 'Nick name is required').not().isEmpty(),
-		check('nickname').custom(existNickname),
-		check('password', 'password is required and its length needs to be more than 6').isLength({
-			min: 6,
-		}),
-		validateFields,
-		validatePassword,
-		validateNickname,
-	],
-	userPost
+  '/',
+  [
+    check('email', 'Invalid email :D').isEmail(),
+    check('email').custom(existEmail),
+    check('fullname', 'Full name is required').not().isEmpty(),
+    check('nickname', 'Nick name is required').not().isEmpty(),
+    check('nickname').custom(existNickname),
+    check(
+      'password',
+      'password is required and its length needs to be more than 6'
+    ).isLength({
+      min: 6,
+    }),
+    validateFields,
+    validatePassword,
+    validateNickname,
+  ],
+  userPost
 );
 
 router.put(
-	'/:id',
-	[
-		validateJWT,
-		check('id', 'Invalid ID').isMongoId(),
-		check('id').custom(existUser),
-		compareJwtInfoAndParamID,
-		validateFields,
-	],
-	usersPut
+  '/:id',
+  [
+    validateJWT,
+    check('id', 'Invalid ID').isMongoId(),
+    check('id').custom(existUser),
+    compareJwtInfoAndParamID,
+    validateFields,
+  ],
+  usersPut
 );
 
 router.delete(
-	'/:id',
-	[
-		validateJWT,
-		check('id', 'Invalid ID').isMongoId(),
-		check('id').custom(existUser),
-		compareJwtInfoAndParamID,
-		validateFields,
-	],
-	userDelete
+  '/:id',
+  [
+    validateJWT,
+    check('id', 'Invalid ID').isMongoId(),
+    check('id').custom(existUser),
+    compareJwtInfoAndParamID,
+    validateFields,
+  ],
+  userDelete
 );
 
 router.put(
-	'/follow/:idUserOne',
-	[validateJWT, check('id', 'Invalid ID').isMongoId(), check('id').custom(existUser)],
-	userFollowing
+  '/follow/:idUserOne',
+  [
+    validateJWT,
+    check('id', 'Invalid ID').isMongoId(),
+    check('id').custom(existUser),
+  ],
+  userFollowing
 );
 
 router.put(
-	'/unfollow/:idUserOne',
-	[validateJWT, check('id', 'Invalid ID').isMongoId(), check('id').custom(existUser)],
-	userUnfollowing
+  '/unfollow/:idUserOne',
+  [
+    validateJWT,
+    check('id', 'Invalid ID').isMongoId(),
+    check('id').custom(existUser),
+  ],
+  userUnfollowing
 );
 
 router.put(
-	'/followers/:id',
-	[validateJWT, check('id', 'Invalid ID').isMongoId(), check('id').custom(existUser)],
-	userFollowers
+  '/followers/:id',
+  [
+    validateJWT,
+    check('id', 'Invalid ID').isMongoId(),
+    check('id').custom(existUser),
+  ],
+  userFollowers
 );
 
 router.put(
-	'/mute/:id',
-	[validateJWT, check('id', 'Invalid ID').isMongoId(), check('id').custom(existUser)],
-	userSilenced
+  '/mute/:id',
+  [
+    validateJWT,
+    check('id', 'Invalid ID').isMongoId(),
+    check('id').custom(existUser),
+  ],
+  userSilenced
 );
 
 router.put(
-	'/unmute/:id',
-	[validateJWT, check('id', 'Invalid ID').isMongoId(), check('id').custom(existUser)],
-	userRemoveSilenced
+  '/unmute/:id',
+  [
+    validateJWT,
+    check('id', 'Invalid ID').isMongoId(),
+    check('id').custom(existUser),
+  ],
+  userRemoveSilenced
 );
 
 router.put(
-	'/block/:id',
-	[validateJWT, check('id', 'Invalid ID').isMongoId(), check('id').custom(existUser)],
-	userBlocked
+  '/block/:id',
+  [
+    validateJWT,
+    check('id', 'Invalid ID').isMongoId(),
+    check('id').custom(existUser),
+  ],
+  userBlocked
 );
 
 router.put(
-	'/unblock/:id',
-	[validateJWT, check('id', 'Invalid ID').isMongoId(), check('id').custom(existUser)],
-	userRemoveBlocked
+  '/unblock/:id',
+  [
+    validateJWT,
+    check('id', 'Invalid ID').isMongoId(),
+    check('id').custom(existUser),
+  ],
+  userRemoveBlocked
 );
 
 router.get('/all/mails', usersMailsAndNicknames);
+
+router.get('/autocomplete/:nickname', queryUsers);
 
 // Mati ama bad bunny
 
