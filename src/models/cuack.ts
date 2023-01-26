0316541f4ad54abb6f722e0e6ad22a8030b6b83f
Matias Straface <matiasgstraface@gmail.com>
2023-01-26 15:47:32 -0300

    cuack model files field created

--- src/models/cuack.ts
+++ src/models/cuack.ts
@@ -1,27 +1,28 @@
 import { Schema, model } from 'mongoose';
 
 const cuackSchema = new Schema({
-  type: { type: String, enum: ['cuack', 'comment'], default: 'cuack' },
-  author: { type: String, trim: true, required: true },
-  content: { type: String, trim: true, required: true },
-  likes: [{ type: String }],
-  recuacks: [{ type: String }],
-  date: { type: Date, default: Date.now },
-  reports: [{ type: String }],
-  comments: [
-    {
-      type: { type: String, default: 'comment' },
-      before: { type: String },
-      author: { type: String, trim: true, required: true },
-      content: { type: String, trim: true, required: true },
-      likes: [{ type: String }],
-      recuacks: [{ type: String }],
-      date: { type: Date, default: Date.now },
-      reports: { type: String },
-    },
-  ],
-  category: { type: String, default: '@Test' },
-  isPublic: { type: Boolean, default: true },
+	type: { type: String, enum: ['cuack', 'comment'], default: 'cuack' },
+	author: { type: String, trim: true, required: true },
+	content: { type: String, trim: true, required: true },
+	likes: [{ type: String }],
+	recuacks: [{ type: String }],
+	date: { type: Date, default: Date.now },
+	reports: [{ type: String }],
+	comments: [
+		{
+			type: { type: String, default: 'comment' },
+			before: { type: String },
+			author: { type: String, trim: true, required: true },
+			content: { type: String, trim: true, required: true },
+			likes: [{ type: String }],
+			recuacks: [{ type: String }],
+			date: { type: Date, default: Date.now },
+			reports: { type: String },
+		},
+	],
+	category: { type: String, default: '@Test' },
+	isPublic: { type: Boolean, default: true },
+	files: [{ type: String }],
 });
 
 export default model('Cuack', cuackSchema);
