77c4610e95b14a04f332bb9f7b63c2b3ac267afc
Matias Straface <matiasgstraface@gmail.com>
2023-02-28 10:46:18 -0300

    fix-notification-content

--- src/controllers/notifications.ts
+++ src/controllers/notifications.ts
@@ -2,27 +2,27 @@ import User from '../models/user';
 import { getMentions } from './utils/getMentions';
 
 export async function addNotification(user: string, content: object) {
-  if (!user || !content) return { reponse: false, payload: 'Missing data' };
-  try {
-    const operation = await User.findOneAndUpdate(
-      { nickname: user },
-      { $push: { notifications: content } }
-    );
-    if (!operation) return { response: false, payload: 'Invalid user' };
-    return { response: true, payload: 'User updated!' };
-  } catch (error) {
-    console.log(`Internal server error ${error}`);
-    return { response: false, payload: 'Internal server error' };
-  }
+	if (!user || !content) return { reponse: false, payload: 'Missing data' };
+	try {
+		const operation = await User.findOneAndUpdate(
+			{ nickname: user },
+			{ $push: { notifications: content } }
+		);
+		if (!operation) return { response: false, payload: 'Invalid user' };
+		return { response: true, payload: 'User updated!' };
+	} catch (error) {
+		console.log(`Internal server error ${error}`);
+		return { response: false, payload: 'Internal server error' };
+	}
 }
 
 export async function notificateMentions(user: any, cuack: any): Promise<void> {
-  const mentions = getMentions(cuack.content);
-  if (mentions?.length) {
-    for (const mention of mentions) {
-      await addNotification(mention, {
-        content: `${user.nickname} te ha mencionado en la publicación ${cuack._id}`,
-      });
-    }
-  }
+	const mentions = getMentions(cuack.content);
+	if (mentions?.length) {
+		for (const mention of mentions) {
+			await addNotification(mention, {
+				content: `${user.nickname} te ha mencionado en una publicación ${cuack._id}`,
+			});
+		}
+	}
 }
